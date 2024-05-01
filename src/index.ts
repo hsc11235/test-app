90335cdca8d897633574a0b389c14aa4bf75044b
Smitty Chandler <hchandler@iralogix.com>
2024-05-01 09:42:43 -0500

    Fix linting errors

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
-import Ajv from "ajv";
 import fastify from "fastify";
-import postgres from "@fastify/postgres";
-import swagger from "@fastify/swagger";
-import swaggerUi from "@fastify/swagger-ui";
 
 const server = fastify({ logger: { level: "info" } });
 
 // GET Route to fetch all users
-server.get('/test', async (_request, _reply) => {
-    return { message: 'Welcome to my Fastify API!' };
-  });
-  
-server.listen({ host: "localhost", port: 5000 }, (err: any, address: any) => {
+server.get("/test", async () => {
+    return { message: "Welcome to my Fastify API!" };
+});
+
+server.listen({ host: "localhost", port: 5000 }, (err, address) => {
     if (err) {
         console.error(err);
         process.exit(1);
